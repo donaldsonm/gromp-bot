b8290f41671de7795f15ee992c3f7f5f6452945c
Michael Donaldson <xmichaeld1@gmail.com>
2020-03-18 16:58:27 -0700

    Added ability to delete commands

--- commands/deletecom.js
+++ commands/deletecom.js
@@ -3,6 +3,27 @@ module.exports = {
     description: "Delete a custom command",
     usage: "-g deletecom [command]",
     execute(bot, message, args, db) {
+        console.log(args);
+        const com_name = args.join(" ");
 
+        db.query(`SELECT * FROM custom_commands WHERE com_name = '${com_name}'`, (err, rows) => {
+                
+            // If there is an error
+            if (err) {
+                throw err;
+            }
+            
+            // If given command name is already in database or in commands folder
+            else if (!rows[0]) {
+                message.channel.send("Command doesn't exist.");
+            }
+
+            // If given command name is unique
+            else {
+                db.query(`DELETE FROM custom_commands WHERE com_name = '${com_name}'`);
+                message.channel.send("Command deleted.");
+            }
+
+        });
     }
 }
